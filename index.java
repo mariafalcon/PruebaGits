6843846421602787a60a150f51c2aa4da83f8aaf
María MAFQ1. Falcón Quintero <mariaa.falcon@softtekprojects.com>
2022-08-04 14:17:22 +0200

    Otra class coche

---
+++ index.java
@@ -0,0 +1,23 @@
+public class Prueba3 {
+
+String nombre = "Maria";
+
+Scanner tec = new Scanner(System.in);
+
+Coche c = new Coche();
+}
+
+
+public class Coche {
+
+
+ String matricula;
+}
+
+public class Coche {
+
+
+ String matricula;
+String dni;s
+
+}
